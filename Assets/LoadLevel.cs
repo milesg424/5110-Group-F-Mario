6e635f02690f438ac6a8441e3eeb1eba26608ea5
milesg424 <98503426+milesg424@users.noreply.github.com>
2024-10-29 09:43:09 -0500

    update

--- Assets/LoadLevel.cs
+++ Assets/LoadLevel.cs
@@ -14,7 +14,7 @@ public class LoadLevel : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.tag == "Player")
-        {
+        {    if(removelv == "Level1") { 
             GameObject.FindGameObjectsWithTag(lv)[0].SetActive(true);
             GameObject.FindGameObjectsWithTag("Player")[0].transform.position = GameObject.FindGameObjectsWithTag("Spawn")[0].transform.position;
             foreach (Transform child in GameObject.FindGameObjectsWithTag(lv)[0].transform)
@@ -23,6 +23,16 @@ public class LoadLevel : MonoBehaviour
             }
             GameObject.FindGameObjectsWithTag(removelv)[0].SetActive(false);
         }
-       
+        if(removelv == "Level2")
+            {
+                
+                GameObject.FindGameObjectsWithTag("Start")[0].SetActive(false);
+                GameObject.FindGameObjectsWithTag("End")[0].transform.GetChild(0).gameObject.SetActive(true);
+                GameObject.FindGameObjectsWithTag("End")[0].transform.GetChild(1).gameObject.SetActive(true);
+               
+               
+                GameObject.FindGameObjectsWithTag("Level2")[0].SetActive(false);
+            }
+        }
     }
 }
